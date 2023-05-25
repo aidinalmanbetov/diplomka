7a20d9e1f3e4199e5ca9e1885f4a7260fa025883
Adin Almanbetov <almanbetovaidin@gmail.com>
2023-05-25 07:24:27 +0600

    fix appeared errors with deleteproductlist

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -6,6 +6,7 @@ import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
 import DeleteProduct from "../DeleteProduct/DeleteProduct";
 
+
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
   
