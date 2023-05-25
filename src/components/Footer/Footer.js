6d79a18e80fc0e0cc6acd9293a14cbdcdc01f6a7
Adin Almanbetov <almanbetovaidin@gmail.com>
2023-05-25 06:34:54 +0600

    changed icon links in footer.js component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,54 +1,46 @@
-import { NavLink } from "react-router-dom";
-import "./Footer.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-function Footer() {
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-menu">
+        <ul className="footer-list">
+          <li>
+            <NavLink to="/" activeClassName="active">Главная</NavLink>
+          </li>
+          <li>
+            <NavLink to="/о-нас" activeClassName="active">О нас</NavLink>
+          </li>
+          <li>
+            <NavLink to="/контакты" activeClassName="active">Контакты</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-contacts">
+        <div className="directions-to-administrator">
+          <h4>Контакты</h4>
+          <p>Адрес: г. Ваш город, ул. Ваша улица, дом 123</p>
+          <p>Телефон: +7 123 456-78-90</p>
+          <p>Email: example@example.com</p>
+        </div>
+      </div>
+      <div className="footer-social">
+        <h4>Мы в социальных сетях</h4>
+        <div className="social-icons">
+          <a href="https://www.whatsapp.com/" target="_blank" rel="noopener noreferrer">
+            <img src="whatsapp-icon.png" alt="WhatsApp" />
+          </a>
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+            <img src="facebook-icon.png" alt="Facebook" />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+            <img src="instagram-icon.png" alt="Instagram" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
 
-return (
-<footer className="footer">
-<div className="footer-container">
-<div className="footer-column">
-<h4 className="footer-heading">Меню</h4>
-<ul className="footer-list">
-<li>
-<NavLink to="/">Главная</NavLink>
-</li>
-<li>
-<NavLink to="/about">О нас</NavLink>
-</li>
-<li>
-<NavLink to="/contact">Контакты</NavLink>
-</li>
-<li>
-<NavLink to="/deliver">Доставка</NavLink>
-</li>
-<li>
-<NavLink to="/orders">Заказы</NavLink>
-</li>
-</ul>
-</div>
-<div className="footer-column">
-<h4 className="footer-heading">Контакты</h4>
-<div className="directions-to-administrator">
-<a className="Tel" target="_blank" rel="noreferrer" href="https://www.google.com/maps?q=42.499292,%2078.412108">Геодезическая 12</a>
-<a className="Tel" target="_blank" rel="noreferrer" href="tel:+996 500 92 37 37">+996 708 76 76 76</a>
-<a className="Tel" target="_blank" rel="noreferrer" href="mailto:orozobekovdosbol07@gmail.com">almanbetovaidin@gmail.com</a>
-</div>
-</div>
-<div className="footer-column">
-<h4 className="footer-heading">Мобильные сети</h4>
-<div className="network-icons">
-<a
-           href="https://api.whatsapp.com/send?phone=996500923737"
-           target="_blank"
-           rel="noreferrer"
-         >
-<svg
-             xmlns="http://www.w3.org/2000/svg"
-             x="0px"
-             y="0px"
-             width="50"
-             height="50"
-             viewBox="0 0 24 24"
-             fill="#fff"
-           >
-<path d="M 12.011719 2 C 6.5057187 2 2.0234844 6.478375 2.0214844 11.984375 C 2.0204844 13.744375 2.4814687 15.462563 3.3554688 16.976562 L 2 22 L 7.2324219 20.763672 C 8.6914219 21.559672 10.333859 21.977516 12.005859 21.978516 L 12.009766 21.978516 C 17.514766 21.978516 21.995047 17.499141 21.998047 11.994141 C 22.000047 9.3251406 20.962172 6.8157344 19.076172 4.9277344 C 17.190172 3.0407344 14.683719 2.001 12.011719 2 z M 12.009766 4 C 14.145766 4.001 16.153109 4.8337969 17.662109 6.3417969 C 19.171" )}
+export default Footer;
