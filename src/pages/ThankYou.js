71bf34d2e4fea2418dac54ea409d7aa200f77916
Adin Almanbetov <almanbetovaidin@gmail.com>
2023-05-25 16:58:38 +0600

    changed the timeline of the transition to the main screen

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -1,7 +1,7 @@
 export default function ThankYou() {
   setTimeout(() => {
     window.location.href = "/";
-  }, 2000);
+  }, 500);
   return (
     <div className="ThankYou">
       <h1>Thank you!</h1>
