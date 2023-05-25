2ea3dbb3be76faa3505553a75cd4259e79d96103
Adin Almanbetov <almanbetovaidin@gmail.com>
2023-05-25 17:00:18 +0600

    removed invalid URL of gif link

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -6,7 +6,7 @@ export default function ThankYou() {
     <div className="ThankYou">
       <h1>Thank you!</h1>
       <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
-      <img src="https://www.funimada.com/assets/images/cards/big/thank-you-2.gif" alt="dsds" width={250} />
+      <img src="" alt="dsds" width={250} />
     </div>
   );
 }
