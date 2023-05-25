4471eaaa178b07437de2a71834e5d361f119aa08
Adin Almanbetov <almanbetovaidin@gmail.com>
2023-05-25 16:42:24 +0600

    I start fixing bugs in the ntfound.js component

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,7 @@ export default function NotFound() {
     <div className="NotFound">
       <h1>404</h1>
       <h2>Everyone left to eat ashlyam-fu</h2>
-      <img src="https://i.gifer.com/origin/6f/6fc964b518401f22cc59707a8c9ca6ee_w200.gif" alt="error" />
+      
     </div>
   )
 }
