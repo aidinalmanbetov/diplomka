cfa1c29868b4a8b25e1d95ff4700bb39983fd367
Adin Almanbetov <almanbetovaidin@gmail.com>
2023-05-25 10:13:51 +0600

    adding CORS d firebase rules

--- src/firebase.js
+++ src/firebase.js
@@ -10,8 +10,6 @@ import {
   signOut,
 } from "firebase/auth";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -28,8 +26,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 const auth = getAuth(app);
